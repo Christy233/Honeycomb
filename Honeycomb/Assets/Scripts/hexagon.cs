eee313762acf41725ac0b737b92511b97230be07
ChrityWu <wukefan233@gmail.com>
2021-01-27 16:34:56 -0800

    -create Hexagon grid
    -crete Hexagon grid
    -add hexagon sprite
    -add neighbors to each hexagon

---
+++ Honeycomb/Assets/Scripts/hexagon.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hexagon : MonoBehaviour
+{
+    hexagon father = null;
+    public hexagon[] neighbors = new hexagon[6];
+    public bool isPass = true;
+    private float gValue = 999f;
+    private float hValue = 999f;
+
+    public void reset()
+    {
+
+    }
+
+    public hexagon[] getNeighborList()
+    {
+        return neighbors;
+    }
+
+    public void setFatherHexagon(hexagon f)
+    {
+        father = f;
+    }
+
+    public hexagon getFatherHexagon()
+    {
+        return father;
+    }
+
+    public void setCanPass(bool f)
+    {
+        isPass = f;
+    }
+
+    public bool canPass()
+    {
+        return isPass;
+    }
+
+    public float computeGValue(hexagon hex)
+    {
+        return 1f;
+    }
+
+    public void setgValue(float v)
+    {
+        gValue = v;
+    }
+
+    public float getgValue()
+    {
+        return gValue;
+    }
+
+    public void sethValue(float v)
+    {
+        hValue = v;
+    }
+
+    public float gethValue()
+    {
+        return hValue;
+    }
+
+    public float computeHValue(hexagon hex)
+    {
+        return Vector3.Distance(transform.position, hex.transform.position);
+    }
+}
+
